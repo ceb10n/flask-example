800f7b52b221771c5bdd981adb6cc3b1fcfe3c76
Rafael Marques <rafaelomarques@gmail.com>
2018-09-21 16:56:11 -0300

    added size object

---
+++ digitalocean/size.py
@@ -0,0 +1,11 @@
+class Size:
+
+    def __init__(self):
+        self.slug = None
+        self.memory = None
+        self.vcpus = None
+        self.disk = None
+        self.transfer = None
+        self.price_monthly = None
+        self.price_hourly = None
+        self.regions = []
