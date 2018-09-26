bec77a4dc5cb2e00479bc25c92d34f3ebf949b75
Rafael Marques <rafaelomarques@gmail.com>
2018-09-26 19:42:52 -0300

    adicionado python-digitalocean

--- api.py
+++ api.py
@@ -1,16 +1,30 @@
-from flask import Flask, jsonify
-from digitalocean import DigitalOcean
+from flask import Flask, render_template
+import digitalocean
 import os
+
 
 app = Flask(__name__)
 do_token = os.environ.get('DO_TOKEN', '')
+digital_ocean = digitalocean.Manager(token=do_token)
 
-@app.route("/account")
-def home():
-    digital_ocean = DigitalOcean(do_token)
-    account = digital_ocean.account()
-    print(account)
-    return jsonify(account.__dict__)
+
+@app.route("/droplets")
+def droplets():
+    droplets = digital_ocean.get_all_droplets()
+    print(droplets)
+    return render_template('droplets.html', droplets=droplets)
+
 
 if __name__ == "__main__":
     app.run()
+
+
+
+
+
+
+
+
+
+
+
