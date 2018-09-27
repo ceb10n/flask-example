7fdaffc293af5d941dc45ebeaee76bd4942d3489
Rafael Marques <rafaelomarques@gmail.com>
2018-09-27 14:29:28 -0300

    refatoracao para o post

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ digital_ocean = digitalocean.Manager(token=do_token)
 @app.route("/droplets")
 def droplets():
     droplets = digital_ocean.get_all_droplets()
-    print(droplets)
+    
     return render_template('droplets.html', droplets=droplets)
 
 
